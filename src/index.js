16fb60c37f7213bc1e7d1b8c54e2375405edccd8
nonuabi <nonuabi@gmail.com>
2021-09-02 16:27:22 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {
-  Products,
-  NewProduct,
-  Navbar,
-  InformationContainer,
-  CartContainer,
-} from "./components/index";
 // GET PROVIDER FROM THE REACT REDUX
 import { Provider } from "react-redux";
 // IMPORT STORE
 import store from "./redux/store";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 ReactDOM.render(
   // SEDING STORE AS A PROPS
   <Provider store={store}>
     <Router>
-      <Navbar />
-      {/* USED SWITCH  */}
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/new">
-          <NewProduct />
-        </Route>
-        <Route path="/productInformation">
-          <InformationContainer />
-        </Route>
-        <Route path="/cartComponent">
-          <CartContainer />
-        </Route>
-        
-      </Switch>
+      <App />
     </Router>
   </Provider>,
 
